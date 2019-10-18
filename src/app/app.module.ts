75722fc93bd4f16a10ec103e94aedcf63b3023cf
Andres Linarez <andreslinarez9@gmail.com>
2019-10-18 13:39:05 -0400

    Se crearon los componentes, servicios y guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { DestinationsComponent } from './components/destinations/destinations.component';
+import { HotelsComponent } from './components/hotels/hotels.component';
+import { HotelComponent } from './components/hotel/hotel.component';
+import { DestinationComponent } from './components/destination/destination.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ItineraryComponent } from './components/itinerary/itinerary.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { PlanYourTripComponent } from './components/plan-your-trip/plan-your-trip.component';
+import { LoginComponent } from './components/admin/login/login.component';
+import { RegisterComponent } from './components/admin/register/register.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DestinationsComponent,
+    HotelsComponent,
+    HotelComponent,
+    DestinationComponent,
+    ContactComponent,
+    ItineraryComponent,
+    PaymentComponent,
+    PlanYourTripComponent,
+    LoginComponent,
+    RegisterComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
