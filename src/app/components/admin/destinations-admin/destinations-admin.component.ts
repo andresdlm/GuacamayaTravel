4b799d94d589a43498c714d09d08921893adeb3a
Andres Linarez <andreslinarez9@gmail.com>
2019-11-14 18:03:08 -0400

    Ya se puede subir una imagen a destinos

--- src/app/components/admin/destinations-admin/destinations-admin.component.ts
+++ src/app/components/admin/destinations-admin/destinations-admin.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DataApiService } from 'src/app/services/data-api.service';
 import { DestinationInterface } from 'src/app/models/destination';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { NgForm } from '@angular/forms'
+import { Button } from 'protractor';
 
 @Component({
   selector: 'app-destinations-admin',
@@ -10,12 +12,38 @@ import { NgForm } from '@angular/forms'
 })
 export class DestinationsAdminComponent implements OnInit {
 
-  constructor(private dataApi: DataApiService) { }
+  constructor(private dataApi: DataApiService, private storage: AngularFireStorage) { }
+
+  @ViewChild('btnFile', {static: false}) btnFile: ElementRef;
+  @ViewChild('btnSubmit', {static: false}) btnSubmit: ElementRef;
 
   private destinations: DestinationInterface[];
 
   ngOnInit() {
     this.getListDestinations();
+  }
+
+  onClick(): void {
+    this.btnFile.nativeElement.click();
+  }
+
+  onUpload(e, destination): void {
+    const id = Math.random().toString(36).substring(2);
+    const file = e.target.files[0];
+    const filePath = `destinos/${id}`;
+    this.storage.ref(filePath);
+    this.storage.upload(filePath, file);
+    destination.imgSRC = filePath;
+    // LLama al boton submit
+    this.btnSubmit.nativeElement.click();
+  }
+
+  saveIMG(destinationForm: NgForm, src: string, id: string): void {
+    // Recibe un NgForm con el valor de la imagen
+    destinationForm.value.imgSRC = src;
+    destinationForm.value.id = id;
+    this.dataApi.updateDestination(destinationForm.value);
+    alert("Se ha subido la imagen");
   }
 
   getListDestinations(): void {
--- src/app/models/destination.ts
+++ src/app/models/destination.ts
@@ -8,4 +8,5 @@ export interface DestinationInterface{
     state?: string;
     city?: string;
     address?: string;
+    imgSRC?: string;
 }
