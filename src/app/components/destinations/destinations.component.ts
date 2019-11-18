240dbc1f0b3e8aed1fc8321a056548d202b5db02
Andres Linarez <andreslinarez9@gmail.com>
2019-11-18 13:26:17 -0400

    Listo los hover de destinos y planea tu viaje

--- src/app/components/destinations/destinations.component.ts
+++ src/app/components/destinations/destinations.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from '../../services/data-api.service';
+import { CategoryInterface } from '../../models/category';
+import { StateInterface } from '../../models/state';
 
 @Component({
   selector: 'app-destinations',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DestinationsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataApi: DataApiService) { }
+
+  private categories: CategoryInterface[];
+  private states: StateInterface[];
 
   ngOnInit() {
+    this.getListCategories();
+    this.getListStates();
+  }
+
+  getListCategories(): void {
+    this.dataApi.readAllCategory().subscribe(categories => {
+      this.categories = categories;
+    });
+  }
+
+  getListStates(): void {
+    this.dataApi.readAllState().subscribe(states => {
+      this.states = states;
+    });
   }
 
 }
